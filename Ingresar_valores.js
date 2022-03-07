ee4e155330e8958f17d387ff06650e64cededa86
Maria Cecilia Sangineto Ruibal <mariacecilia.sanginetoruibal@endava.com>
2022-03-07 12:26:27 -0300

    Update Ingresar_valores.js

--- Ingresar_valores.js
+++ Ingresar_valores.js
@@ -1,5 +1,6 @@
 // Estoy en la branch dev
 //Hola estoy con santi
+// ceci
 var prompt = require('prompt-sync')();
 var n = prompt('Ingresa un valor por pantalla: ');
 console.log("El valor que ingresaste es : " + n);
